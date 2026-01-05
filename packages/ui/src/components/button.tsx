e20f6afbc56e317cce94a93184f6d0540dea917f
Hashversion <110537156+Hashversion@users.noreply.github.com>
2026-01-05 16:06:09 +0530

    style: button componenet

--- packages/ui/src/components/button.tsx
+++ packages/ui/src/components/button.tsx
@@ -3,5 +3,17 @@
 import { cn } from "@repo/ui/lib/utils";
 
 export const Button = ({ children, className }: React.ComponentProps<"button">) => {
-  return <button className={cn("bg-neutral-300 text-neutral-950", className)}>{children}</button>;
+  return (
+    <button
+      className={cn(
+        "bg-neutral-300 text-neutral-950",
+        "rounded-full px-2.5 py-1",
+        "hover:bg-neutral-400",
+        "duration-300",
+        className
+      )}
+    >
+      {children}
+    </button>
+  );
 };
