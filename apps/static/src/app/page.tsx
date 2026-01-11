ce9660a2762eb65a89fd6d30b675187b94287123
Hashversion <110537156+Hashversion@users.noreply.github.com>
2026-01-12 02:54:44 +0530

    feat: enhance home page layout with logo and update icon component

--- apps/static/src/app/page.tsx
+++ apps/static/src/app/page.tsx
@@ -1,10 +1,16 @@
 import { Button } from "@repo/ui/components/button";
+import { Icons } from "@repo/ui/components/icons";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl font-semibold">Koes</h1>
-      <Button>it&apos;s nice</Button>
-    </div>
+    <header className="py-8">
+      <div className="mx-auto max-w-300 px-3 lg:px-0">
+        <div className="flex items-center justify-center gap-4">
+          <Icons.Logo className="size-8" />
+          <h1 className="text-3xl font-semibold">KOES</h1>
+        </div>
+        <Button>it&apos;s nice</Button>
+      </div>
+    </header>
   );
 }
