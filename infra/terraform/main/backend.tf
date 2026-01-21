c1e56a9e33f3e934344094eeb0832e7072b90c33
Hashversion <110537156+Hashversion@users.noreply.github.com>
2026-01-21 23:53:03 +0530

    feat: add lifecycle rule to prevent destruction of terraform state S3 bucket

--- infra/terraform/main/backend.tf
+++ infra/terraform/main/backend.tf
@@ -1,6 +1,10 @@
 resource "aws_s3_bucket" "terraform_state" {
   bucket = var.terraform_state_bucket
 
+  lifecycle {
+    prevent_destroy = true
+  }
+
   tags = merge(local.common_tags, {
     Name        = "Terraform State Bucket"
     Description = "Central S3 bucket for storing Terraform state files"
