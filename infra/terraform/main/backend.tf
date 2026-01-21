5244e0d9dacf33a5b0b32923e45056058b8f227e
Hashversion <110537156+Hashversion@users.noreply.github.com>
2026-01-21 21:46:45 +0530

    feat: enable versioning for Terraform state S3 bucket

--- infra/terraform/main/backend.tf
+++ infra/terraform/main/backend.tf
@@ -1,9 +1,17 @@
 resource "aws_s3_bucket" "terraform_state" {
   bucket = var.terraform_state_bucket
 
-
   tags = merge(local.common_tags, {
     Name        = "Terraform State Bucket"
     Description = "Central S3 bucket for storing Terraform state files"
   })
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+
 }
