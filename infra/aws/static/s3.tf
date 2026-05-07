eb4cc1ebe62beec0439316a2cbcf5fa84a71a992
Hashversion <110537156+Hashversion@users.noreply.github.com>
2026-05-08 03:12:45 +0530

    fix: update S3 bucket policy structure

--- infra/aws/static/s3.tf
+++ infra/aws/static/s3.tf
@@ -79,21 +79,21 @@ resource "aws_s3_bucket_public_access_block" "koes_static" {
 resource "aws_s3_bucket_policy" "cloudflare_ips_only" {
   bucket = aws_s3_bucket.koes_static.id
   policy = jsonencode({
-    sid       = "PublicReadGetObject"
-    effect    = "allow"
-    principal = "*"
-    actions = [
-      "s3:GetObject",
-    ]
-    resources = [
-      "${aws_s3_bucket.koes_static.arn}/*",
-    ]
-    condition = {
-      IpAddress = {
-        "aws:SourceIp" = local.cloudflare_ips
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = ["s3:GetObject"]
+        Resource  = ["${aws_s3_bucket.koes_static.arn}/*"]
+        Condition = {
+          IpAddress = {
+            "aws:SourceIp" = local.cloudflare_ips
+          }
+        }
       }
-    }
-    }
-  )
+    ]
+  })
 }
 
