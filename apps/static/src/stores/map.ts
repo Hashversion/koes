0fbdf7a406e641a6a53633397e81234575eb94e1
Hashversion <110537156+Hashversion@users.noreply.github.com>
2026-05-08 23:47:12 +0530

    feat: update initial nodes and edges in map store

--- apps/static/src/stores/map.ts
+++ apps/static/src/stores/map.ts
@@ -3,11 +3,29 @@ import { addEdge, applyEdgeChanges, applyNodeChanges } from "@xyflow/react";
 import { create } from "zustand";
 
 const initialNodes: MapNode[] = [
-  { id: "1", data: { label: "AWS" }, position: { x: 1, y: 1 } },
-  { id: "2", data: { label: "Cloudflare" }, position: { x: 1, y: 200 } },
+  { id: "0", data: { label: "GitHub Actions" }, position: { x: -300, y: -200 }, type: "input" },
+  { id: "1", data: { label: "AWS OIDC (main acc)" }, position: { x: -300, y: -80 } },
+  { id: "2", data: { label: "AWS (static acc)" }, position: { x: -300, y: 30 } },
+  { id: "3", data: { label: "S3 Bucket" }, position: { x: -300, y: 130 }, type: "output" },
 ];
 
-const initialEdges: MapEdge[] = [{ id: "e1-2", source: "1", target: "2" }];
+const initialEdges: MapEdge[] = [
+  {
+    id: "e0-1",
+    label: "assume role",
+    source: "0",
+    target: "1",
+    markerEnd: { type: "arrow" },
+  },
+  {
+    id: "e1-2",
+    source: "1",
+    target: "2",
+    markerEnd: { type: "arrow" },
+    label: "assume role",
+  },
+  { id: "e2-3", source: "2", target: "3", markerEnd: { type: "arrow" } },
+];
 
 export const useMapStore = create<MapState>((set) => ({
   nodes: initialNodes,
