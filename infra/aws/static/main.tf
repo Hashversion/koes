654a8c11cc5c86b9a889dadb9849b7e12c92553b
Hashversion <110537156+Hashversion@users.noreply.github.com>
2026-05-08 13:40:42 +0530

    feat: remove data classification entry from common_tags

--- infra/aws/static/main.tf
+++ infra/aws/static/main.tf
@@ -1,12 +1,11 @@
 locals {
   common_tags = {
-    Project            = "KOES"
-    app                = "static"
-    Environment        = "Development"
-    ManagedBy          = "Terraform"
-    Component          = "static-site"
-    Repository         = "hashversion/koes"
-    DataClassification = "public"
+    Project     = "KOES"
+    app         = "static"
+    Environment = "Development"
+    ManagedBy   = "Terraform"
+    Component   = "static-site"
+    Repository  = "hashversion/koes"
   }
 
 }
